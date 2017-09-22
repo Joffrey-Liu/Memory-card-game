2d785dc6568276cdd3ad355ecb0866d1a5e081ed
Joffrey-Liu <32201219+Joffrey-Liu@users.noreply.github.com>
2017-09-22 23:39:48 +0800

    C# version

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace 視窗HW5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(53, 20);
+            this.File.Text = "Game";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "Restart";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 224);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 37);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0 second";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 視窗HW5
+{
+    public partial class Form1 : Form
+    {
+        Rectangle[] RectDest = new Rectangle[17];
+        Boolean[] RectMatch = new Boolean[17];
+        Boolean Click = true;
+        int[,] Compare = new int[8, 2] { { 1, 11 }, { 2, 10 }, { 3, 8 }, { 4, 6 }, { 5, 14 }, { 7, 12 }, { 9, 16 }, { 13, 15 } };
+        int[] CurrentClick = new int[2];
+        Image All = Properties.Resources._8_0;
+        Image Apple = Properties.Resources._8_1;
+        Image Banana = Properties.Resources._8_8;
+        Image Cherry = Properties.Resources._8_4;
+        Image Grape = Properties.Resources._8_3;
+        Image Orange = Properties.Resources._8_5;
+        Image Pieapple = Properties.Resources._8_6;
+        Image Tomato = Properties.Resources._8_2;
+        Image Watermelon = Properties.Resources._8_7;
+        int time = 0;
+        int x = 0, y = 30;
+        int clickcount = 0;
+        int filptime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(500, 500);
+            timer1.Interval = 1000;
+            timer1.Start();
+            CurrentClick[0] = 0;
+            CurrentClick[1] = 0;
+            for (int i = 1; i < 17; i++)
+                RectMatch[i] = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            label1.Text = time + " second";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (ImageExsist())
+            {
+                x = 0; y = 30;
+                for (int i = 1; i < 17; i++)
+                {
+                    RectDest[i] = new Rectangle(x, y, All.Width, All.Height);
+                    x += All.Width;
+                    if (i % 4 == 0)
+                    {
+                        x = 0;
+                        y += All.Height;
+                    }
+                    e.Graphics.DrawImage(All, RectDest[i]);
+                    e.Graphics.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                }
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Click)
+            {
+                clickcount++;
+                for (int i = 1; i < 17; i++)
+                {
+                    if (RectDest[i].Contains(e.Location))
+                    {
+                        RectMatch[i] = true;
+                        Graphics g = this.CreateGraphics();
+                        switch (i)
+                        {
+                            case 1:
+                                {
+                                    g.DrawImage(Apple, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 2:
+                                {
+                                    g.DrawImage(Banana, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 3:
+                                {
+                                    g.DrawImage(Cherry, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 4:
+                                {
+                                    g.DrawImage(Orange, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 5:
+                                {
+                                    g.DrawImage(Grape, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 6:
+                                {
+                                    g.DrawImage(Orange, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 7:
+                                {
+                                    g.DrawImage(Pieapple, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 8:
+                                {
+                                    g.DrawImage(Cherry, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 9:
+                                {
+                                    g.DrawImage(Tomato, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 10:
+                                {
+                                    g.DrawImage(Banana, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 11:
+                                {
+                                    g.DrawImage(Apple, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 12:
+                                {
+                                    g.DrawImage(Pieapple, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 13:
+                                {
+                                    g.DrawImage(Watermelon, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 14:
+                                {
+                                    g.DrawImage(Grape, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 15:
+                                {
+                                    g.DrawImage(Watermelon, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                            case 16:
+                                {
+                                    g.DrawImage(Tomato, RectDest[i]);
+                                    g.DrawRectangle(new Pen(Color.Black, 3), RectDest[i]);
+                                    RectMatch[i] = true;
+                                    CurrentClick[clickcount - 1] = i;
+                                    timer2.Start();
+                                }
+                                break;
+                        }
+                        if (clickcount == 2)
+                        {
+                            clickcount = 0;
+                            Click = false;
+                        }
+                    }
+                }
+            }
+        }
+            Boolean ImageExsist()
+        {
+            if (All != null && Apple != null && Banana != null && Cherry != null && Grape != null && Orange != null && Pieapple != null && Tomato != null && Watermelon != null)
+                return true;
+            else
+                return false;
+        }
+
+        Boolean Match(int []input)
+        {
+            for(int i=0; i<8; i++)
+            {
+                if ((input[0] == Compare[i, 0] && input[1] == Compare[i, 1]) || input[1] == Compare[i, 0] && input[0] == Compare[i, 1])
+                {
+                    return true; 
+                }
+            }
+            return false;
+        }
+
+        Boolean AllMatch()
+        {
+            if (RectMatch[1] == true && RectMatch[11] == true && RectMatch[2] == true && RectMatch[10] == true && RectMatch[3] == true && RectMatch[8] == true && RectMatch[4] == true && RectMatch[6] == true &&
+                RectMatch[5] == true && RectMatch[14] == true && RectMatch[7] == true && RectMatch[12] == true && RectMatch[9] == true && RectMatch[16] == true && RectMatch[13] == true && RectMatch[15] == true)
+                return true;
+            else
+                return false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+         filptime=0;
+            filptime++;
+            Graphics g = this.CreateGraphics();
+            if (CurrentClick[0] != 0 && CurrentClick[1] != 0)
+            {
+                if (Match(CurrentClick) == false)
+                {
+                    timer2.Interval = 1000;
+                    timer2.Start();
+                    if (filptime == 1)
+                    {
+                        Click = false;
+                        filptime = 0;
+                        timer2.Stop();
+                        g.DrawImage(All, RectDest[CurrentClick[0]]);
+                        g.DrawRectangle(new Pen(Color.Black, 3), RectDest[CurrentClick[0]]);
+                        g.DrawImage(All, RectDest[CurrentClick[1]]);
+                        g.DrawRectangle(new Pen(Color.Black, 3), RectDest[CurrentClick[1]]);
+                    }
+                    RectMatch[CurrentClick[0]] = false;
+                    RectMatch[CurrentClick[1]] = false;
+                    CurrentClick[0] = 0;
+                    CurrentClick[1] = 0;
+                }
+                else if (Match(CurrentClick) == true)
+                {
+                    RectMatch[CurrentClick[0]] = true;
+                    RectMatch[CurrentClick[1]] = true;
+                    CurrentClick[0] = 0;
+                    CurrentClick[1] = 0;
+                    filptime = 0;
+                }
+            }
+            if (AllMatch())
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+            Click = true;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+             this.Invalidate();
+            time = 0;
+            x = 0; y = 30;
+            clickcount = 0;
+            filptime = 0;
+            for (int i = 1; i < 17; i++)
+                RectMatch[i] = false;
+            CurrentClick[0] = 0; CurrentClick[1] = 0;
+            timer1.Interval = 1000;
+            timer1.Start();
+            Click = true;
+        }
+        
+    }
+}
